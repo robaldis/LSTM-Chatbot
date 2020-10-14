844528b6447be12848ce5695d1c75c3962411ef7
robaldis <robertaldis@gmail.com>
2020-10-14 21:31:50 +0100

    running some code

--- src/data_cleaning/human-bot_dataclean.py
+++ src/data_cleaning/human-bot_dataclean.py
@@ -1,3 +1,7 @@
+import re
+
+line_limit = 1000
+
 def refactoring_data():
     data_path = "assets/human_text.txt"
     data_path2 = "assets/robot_text.txt"
@@ -6,10 +10,10 @@ def refactoring_data():
         lines = f.read().split('\n')
     with open(data_path2, 'r', encoding='utf-8') as f:
         lines2 = f.read().split('\n')
-    lines = [re.sub(r"\[\w+\]",'hi',line) for line in lines]
+    lines = [re.sub(r"\[\w+\]",'hi',line) for line in lines[:line_limit]]
     lines = [" ".join(re.findall(r"\w+",line)) for line in lines]
     lines2 = [re.sub(r"\[\w+\]",'',line) for line in lines2]
-    lines2 = [" ".join(re.findall(r"\w+",line)) for line in lines2]
+    lines2 = [" ".join(re.findall(r"\w+",line)) for line in lines2[:line_limit]]
     # grouping lines by response pair
     pairs = list(zip(lines,lines2))
     #random.shuffle(pairs)
@@ -18,3 +22,5 @@ def refactoring_data():
             f.writelines(line[0] + '\t' + line[1] + '\n')
 
 
+if __name__ == "__main__":
+    refactoring_data()
